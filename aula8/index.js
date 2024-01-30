3eb85b8fbe3d1ef88a0d9eabec0baf36b5da9ef8
alicebelchior <alice.belchior23@gmail.com>
2024-01-29 23:31:39 -0300

    prática da aula 8

---
+++ aula8/index.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const porta = 443;
+
+const servidor = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-type": "text/html" });
+
+  // a requisição no servidor for  pagina raiz,
+  if ((req.url = "/")) {
+    // sera gerado uma resposta
+    res.write("<h1>Seja bem vindo!</h1>");
+  }
+  //Outro caminho (rota)
+  else if ((req.url = "/server")) {
+    res.write("<h1>Servidor</h1>");
+  } else if ((req.url = "/server/node")) {
+    res.write("<h1>Servidor Web no Node</h1>");
+  } else {
+    res.write("<h1>PADRÃO</h1>");
+  }
+});
+
+servidor.listen(porta, () => {
+  console.log("Servidor rodando");
+});
